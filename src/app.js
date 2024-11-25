8369d5bf240d953df738b4c591868eeeaead7111
Adiregde <adiregde118@gmail.com>
2024-11-25 19:03:36 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -3,17 +3,22 @@ const express = require('express');
 
 const app = express();
 
-app.use("/home",(req,res)=>{
-    res.send('hello wokring on 3006');
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Aditya", lastName: "Regde"});
 })
+
+app.post("/user", (req,res)=>{
+    res.send('Data Successfully saved the database');
+});
+
+app.delete("/user", (req,res)=>{
+    res.send('Data deleted successfully');
+});
 
 app.use("/test",(req,res)=>{
     res.send('hello wokring on test');
 })
 
-app.use("/",(req,res)=>{
-    res.send('hello wokring on moan page');
-})
 
 app.listen(3006, ()=>{
     console.log('hello welcoem to 3006');    
